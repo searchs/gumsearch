589e0c9c03414a6616e547acd326ad9cb0a09041
Ola Ajibode <ola.ajibode@gmail.com>
2018-03-25 19:05:00 +0100

    [basic search ] - instruction added to basic search

---
+++ src/test/scala/searches/spec/SearchGoogle.scala
@@ -0,0 +1,8 @@
+package searches.spec
+
+class SearchGoogle {
+//  1.	Open Google and perform a search for ‘Cars in London’.
+//  2.	Get all search results displayed and validate how many Gumtree links there are available (you may only focus on the first page of the search results for the purpose of this task).
+//  3.	Navigate to each Gumtree link and confirm the title is displayed and the number of the results is greater than 0.
+
+}
